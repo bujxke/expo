4f6dfba559d8d5fae1f238f1add650ae957f2364
Kudo Chien <kudo@expo.dev>
2026-04-11 03:16:27 +0800

    [ui] add expand method to ModalBottomSheet

--- packages/expo-ui/android/src/main/java/expo/modules/ui/ExpoUIModule.kt
+++ packages/expo-ui/android/src/main/java/expo/modules/ui/ExpoUIModule.kt
@@ -133,6 +133,12 @@ class ExpoUIModule : Module() {
       AsyncFunction("hide") Coroutine { view: ModalBottomSheetView ->
         view.hide()
       }
+      AsyncFunction("expand") Coroutine { view: ModalBottomSheetView ->
+        view.expand()
+      }
+      AsyncFunction("partialExpand") Coroutine { view: ModalBottomSheetView ->
+        view.partialExpand()
+      }
     }
 
     ExpoUIView("SingleChoiceSegmentedButtonRowView") { props: SingleChoiceSegmentedButtonRowProps ->
